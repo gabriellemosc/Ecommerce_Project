fed6b9dab26655e2cfcd039d53b4bfa2b33a02f2
gabriellemosc <gabriel.palacios1910@gmail.com>
2024-11-09 18:16:55 -0300

    Criando Páginas HTML e URLs das Páginas

--- loja/urls.py
+++ loja/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import *     #import all the functions, to run our URL Link from the file views.py   
 
 urlpatterns = [
-    path('',homepage, name='homepage' )             #homepage
+    path('',homepage, name='homepage'),                      #homepage
+    path('loja/',loja, name='loja'),                              #store
+    path('minhaconta/', minha_conta, name='minhaconta'),                    #my account
+    path('login',login, name='login' ),                         #login
+    path('carrinho', carrinho, name='carrinho'),
+    path('checkout',checkout, name='checkout' ),             #checkout
 ]
--- loja/views.py
+++ loja/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
-    #run the homepage view
+
 def homepage(request):              
     return render(request, 'homepage.html')     #load the HTML file
+
+# store
+def loja(request):              
+    return render(request, 'loja.html')     #load the HTML file
+
+
+def carrinho(request):              
+    return render(request, 'carrinho.html')     #load the HTML file
+
+#exit the site
+def checkout(request):              
+    return render(request, 'checkout.html')     #load the HTML file
+
+# Page My account
+def minha_conta(request):              
+    return render(request, 'usuario/minha_conta.html')     #load the HTML file
+#login page
+def login(request):              
+    return render(request, 'usuario/login.html')     #load the HTML file
